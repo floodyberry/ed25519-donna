3d03b38d73a70f1ef33d1d936d2fbacf4bf29b76
Andrew M <liquidsun@gmail.com>
2013-11-04 01:24:39 -0600

    full clobber list

--- ed25519-donna-64bit-x86.h
+++ ed25519-donna-64bit-x86.h
@@ -340,7 +340,10 @@ ge25519_scalarmult_base_choose_niels(ge25519_niels *t, const uint8_t table[256][
 		"movq %%rdi, 112(%2)             ;\n"
 		:
 		: "m"(u), "r"(&table[pos * 8]), "r"(t), "m"(sign) /* %0 = u, %1 = table, %2 = t, %3 = sign */
-		: "%rax", "%rcx", "%rdx", "%rdi", "%rsi", "%r8", "%r9", "%r10", "%r11", "%r12", "%r13"
+		:
+			"%rax", "%rcx", "%rdx", "%rdi", "%rsi", "%r8", "%r9", "%r10", "%r11", "%r12", "%r13", 
+			"%xmm0", "%xmm1", "%xmm2", "%xmm3", "%xmm4", "%xmm5", "%xmm6", "%xmm7", "%xmm8", "%xmm9", "%xmm10", "%xmm11", "%xmm14", "%xmm14",
+			"cc", "memory"
 	);
 }
 
